449945a1b29b34778bc5d1c00a20d008dc590196
Dev4 <Dev4@root.maysam.com>
2021-10-23 15:58:22 +0330

    to down dompleted

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 const sideBar = document.querySelector(".sidebar");
-const profile = document.querySelector(".profile");
-const menu = document.querySelector(".menu");
 
-const content = document.querySelector(".content");
+const vh = window.innerHeight;
 
 const sideBarHeight = sideBar.offsetHeight;
-console.log(sideBarHeight)
+console.log(sideBarHeight);
 
-const contentHeight = document.querySelector(".content").scrollHeight;
+// const contentHeight = document.querySelector(".content").scrollHeight;
 // console.log("content Heigh :" + contentHeight);
 
-const sidebarHeight = document.querySelector(".sidebar").scrollHeight;
+// const sidebarScrollTop = document.querySelector(".sidebar").scrollHeight;
 // console.log("sidebar Height :" + sidebarHeight);
 
-const sideBarTopDistance = sideBar.offsetTop;
-const profileTopDistance = profile.offsetTop;
 
-window.addEventListener("scroll",()=>{
+
+
+
+console.log("sidebar Height : " + sideBarHeight);
+
+function toDown(){
+    const sideBarTopDistance = sideBar.getBoundingClientRect();
+    const sidebarTop = sideBarTopDistance.top;
+
+
     const scrolled = window.scrollY;
     console.log("scrolled :" + scrolled);
-    // fixedSidebar();
-    if (scrolled>=40){
-        sideBar.style.bottom=0+"px";
-        // sideBar.style.top=-40+"px";
-    }    
-})
-// console.log(sideBarTopDistance);
-// console.log(profileTopDistance);
 
+    console.log("sidebar Top distance :" + sidebarTop)
+    if (sideBarHeight+sidebarTop >vh){
+        console.log("456")
+        sideBar.style.top=-scrollY;
+    } else {
+        console.log("123")
+        sideBar.style.top= vh - sideBarHeight;
+    } 
 
-// function fixedSidebar (){
-//     if (window.scrollY>profileTopDistance){
-//         sideBar.style.bottom=0;
-//     }
-// }
+    // if (scrolled > sideBarHeight){
+    //     sideBar.style.bottom = -1000 + "px";
+    // }
+
+    // if (scrolled < 500+"px"){
+    //     sideBar.style.top = -scrolled;
+    // } else {
+    //     sideBar.style.bottom = 0;
+        
+    // }
+}
+window.addEventListener("scroll", toDown);
+toDown();
+
