5e831d91e550ef65b1c03e4f7969a47ddfc39154
Dev4 <Dev4@root.maysam.com>
2021-10-25 09:59:00 +0330

    to up first version

--- script.js
+++ script.js
@@ -5,25 +5,16 @@ const vh = window.innerHeight;
 const sideBarHeight = sideBar.offsetHeight;
 console.log(sideBarHeight);
 
-// const contentHeight = document.querySelector(".content").scrollHeight;
-// console.log("content Heigh :" + contentHeight);
-
-// const sidebarScrollTop = document.querySelector(".sidebar").scrollHeight;
-// console.log("sidebar Height :" + sidebarHeight);
-
-
-
 
 
 console.log("sidebar Height : " + sideBarHeight);
 
+let lastScrollPoitn = 0 ;
+
+
 function toDown(){
     const sideBarTopDistance = sideBar.getBoundingClientRect();
     const sidebarTop = sideBarTopDistance.top;
-
-
-    const scrolled = window.scrollY;
-    console.log("scrolled :" + scrolled);
 
     console.log("sidebar Top distance :" + sidebarTop)
     if (sideBarHeight+sidebarTop >vh){
@@ -32,19 +23,29 @@ function toDown(){
     } else {
         console.log("123")
         sideBar.style.top= vh - sideBarHeight;
-    } 
+    }  
 
-    // if (scrolled > sideBarHeight){
-    //     sideBar.style.bottom = -1000 + "px";
-    // }
 
-    // if (scrolled < 500+"px"){
-    //     sideBar.style.top = -scrolled;
-    // } else {
-    //     sideBar.style.bottom = 0;
-        
-    // }
+
+    function dirDetector(){
+        if(this.oldScroll>this.scrollY){
+            console.log("up")
+            if(sidebarTop>0){
+                sideBar.style.top = scrollY;
+            } else {
+                sideBar.style.top = 0;
+            }
+        } else {
+            console.log("down")
+        }
+        this.oldScroll= this.scrollY;
+    }
+
+    dirDetector();
 }
 window.addEventListener("scroll", toDown);
-toDown();
+
+
+// scroll direction detector
+
 
